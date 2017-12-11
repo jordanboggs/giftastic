0b13acea3e6292a51197a22f00545f17d5fd223d
Jordan Boggs <boggsjp@gmail.com>
2017-12-11 13:40:40 -0700

    changed from .click to .on('click') events

--- assets/app.js
+++ assets/app.js
@@ -1,54 +1,52 @@
 /*
- * G I F T A S T I C
- * Using GIPHY'S API
- * by Jordan Boggs
- * 2017 DU Coding Boot Camp
- */
-$(document).ready(function(){
-  var buttons = ["cat","dog","rabbit","bird","capybara","llama","alpaca",
-                 "hedgehog","echidna","fox"];
+* G I F T A S T I C
+* Using GIPHY'S API
+* by Jordan Boggs
+* 2017 DU Coding Boot Camp
+*/
+var buttons = ["cat","dog","rabbit","bird","capybara","llama","alpaca",
+                "hedgehog","echidna","fox"];
 
-  var renderButtons = function() {
-    $("#buttons").empty();
+var renderButtons = function() {
+  $("#buttons").empty();
 
-    buttons.forEach(element => {
-      $("#buttons").append(`<button class="search-item" data-animal="${element}">${element}</button>`);
-    });
-  };
+  buttons.forEach(element => {
+    $("#buttons").append(`<button class="search-item" data-animal="${element}">${element}</button>`);
+  });
+};
+
+renderButtons();
+
+// This function handles events where a new animal is added
+$(document).on("click", "#add-animal", function(event) {
+  event.preventDefault();
+  let animal = $("#animal-input").val().trim();
+
+  buttons.push(animal);
 
   renderButtons();
+});
 
-  // This function handles events where a new animal is added
-  $("#add-animal").click(function(event) {
-    event.preventDefault();
-    let animal = $("#animal-input").val().trim();
+// This function calls the GIPHY API to display animal gifs
+// $(".search-item").click(function() {
+$(document).on("click", ".search-item", function() {
+  const animal = $(this).attr("data-animal");
 
-    buttons.push(animal);
+  const queryURL = `https://api.giphy.com/v1/gifs/search?q=${animal}&api_key=xglemIPCq6dg9YlSR2vXBBkXTxbgyqUg&limit=10`;
+  
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).done(function(response) {
+    const results = response.data;
 
-    renderButtons();
+    for (let i = 0; i < results.length; i++) {
+      $("#images").prepend(`
+        <div class="result-item">
+          <p>Rating: ${results[i].rating.toUpperCase()}</p>
+          <img src="${results[i].images.fixed_height.url}">
+        </div>
+      `);
+    }
   });
-
-  // This function calls the GIPHY API to display animal gifs
-  $(".search-item").click(function() {
-    const animal = $(this).attr("data-animal");
-
-    const queryURL = `https://api.giphy.com/v1/gifs/search?q=${animal}&api_key=xglemIPCq6dg9YlSR2vXBBkXTxbgyqUg&limit=10`;
-    
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).done(function(response) {
-      const results = response.data;
-
-      for (let i = 0; i < results.length; i++) {
-        $("#images").prepend(`
-          <div class="result-item">
-            <p>Rating: ${results[i].rating.toUpperCase()}</p>
-            <img src="${results[i].images.fixed_height.url}">
-          </div>
-        `);
-      }
-    });
-  });
-
 });
